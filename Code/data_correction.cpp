bd0b9fa8b971e2b0e9396d4363ebc548297e8a78
Liu, Zhirou <zhiroul2@illinois.edu>
2022-03-28 23:08:17 -0500

    Update data_correction.cpp

--- Code/data_correction.cpp
+++ Code/data_correction.cpp
@@ -8,7 +8,7 @@ The function takes input of the filename of the Node data file
 The file has mutliple lines of the following format: NodeID, x coordinate, y coordinate
 In this function, we will take the input, read the input, and store the correct input in a map<NodeID, Coordinate>
 If the Node ID is negative or the coordinates are negative, the value would be excluded.
-If they are mutiple copies of 1 Node ID, only 1 line would be stored.
+If theere are mutiple copies of 1 Node ID, only 1 line would be stored.
 **/
 map<int, Coordinate> nodeData(string filename){
     map<int, Coordinate> vect;
