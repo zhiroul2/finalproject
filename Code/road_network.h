d34e4aaf157354fd88e88371bed31969d087fafd
Liu, Zhirou <zhiroul2@illinois.edu>
2022-04-09 23:33:23 -0500

    Update road_network.h

--- Code/road_network.h
+++ Code/road_network.h
@@ -8,6 +8,27 @@
 
 class RoadNetwork {
     public:
+        /* The constructor for the RoadNetwork class
+        */
+        RoadNetwork(std::map<int, Coordinate> nodeData, std::map<int, EdgeData> edgeData);
+        /* add node to the graph
+        */
+        void addNode(int id, int x, int y);
+        /* add edge to the adjacency list
+        */
+        void addEdge(Node start, Node end, double distance);
+        class Node{
+            public:
+            int NodeID_;
+            int x_;
+            int y_;
+        };
+        class Edge{
+            public:
+            Node* start;
+            Node* end;
+            double distance;
+        };
         /*
         @param the data contains information about the edgeData
         distance function finds the distances between two connected network nodes and sort them into a vector
@@ -16,16 +37,14 @@ class RoadNetwork {
         */
         std::vector<double> distance(std::map<int, EdgeData> data);
         /*
-        @param the vector of coordinates of the nodes, the index represents the nodeID
-        @param the vector distances between the two nodes
         shortestPath comapres the distances between two nodes and finding the start and end node which has the shortest path
         Returns a pair of int with the first being the start nodeID and the second is the end nodeID
         */
-        std::pair<int, int> shortestPath(std::vector<Coordinate> coordinate, std::vector<double> distance);
+        std::pair<int, int> shortestPath();
         
     private:
-        //The map containing the nodeID and its coorsponding x and y coordinates of the road networks
-        std::map<int, Coordinate> nodeData_;
-        //The map map containing the nodeID and its startNode and endNode of the road networks
-        std::map<int, EdgeData> edgeData_;
+        //nodes
+        std::vector<Node*> Nodelist_;
+        //adjacency list
+        list<Edge> *adjLists;
 };
