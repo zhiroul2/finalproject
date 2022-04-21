4e49c9ea82e7c9c72b24d7507732ff253d1ee228
Chan, Muk Yeh <mukyehc2@illinois.edu>
2022-04-20 21:27:19 -0500

    Update roadTest.cpp

--- Code/tests/roadTest.cpp
+++ Code/tests/roadTest.cpp
@@ -1,7 +1,9 @@
 #include "../cs225/catch/catch.hpp"
 #include "../road_network.h"
-
+#include <cmath>
+#include <iostream>
 #include <vector>
+using namespace std;
 
 TEST_CASE("Nodes successfully added to the RoadNetwork class", "[addNode()]") {
     vector<RoadNetwork::Node*> expected;
@@ -30,3 +32,31 @@ TEST_CASE("Nodes successfully added to the RoadNetwork class", "[addNode()]") {
     }
 }
 
+
+double calculate_dist(double ax, double ay, double bx, double by){
+    return sqrt((ax-bx)*(ax-bx) + (ay-by)*(ay-by));
+}
+
+TEST_CASE("correctly find the shortest path of two nodes SIMPLE"){
+    RoadNetwork test_graph = RoadNetwork();
+    test_graph.addNode(1, 2, 2);
+    test_graph.addNode(2, 4, 4);
+    test_graph.addNode(3, 6, 6);
+    test_graph.addNode(4, 8, 8);
+    test_graph.addNode(5, 10, 10);
+    test_graph.addNode(6, 100, 100);
+    test_graph.addNode(7, 2, 100);
+    test_graph.addEdge(1, 2, calculate_dist(2,2,4,4));
+    test_graph.addEdge(2, 3, calculate_dist(4,4,6,6));
+    test_graph.addEdge(3, 4, calculate_dist(6,6,8,8));
+    test_graph.addEdge(4, 5, calculate_dist(8,8,10,10));
+    test_graph.addEdge(5, 6, calculate_dist(10,10,100,100));
+    test_graph.addEdge(1, 7, calculate_dist(2,2,100,100));
+    test_graph.addEdge(6, 7, calculate_dist(100,100,2,100));
+    vector <int> test_path = test_graph.shortestPath(1, 6);
+    vector <int> actual_path;
+    for (int i = 1; i < 7; i++){
+        actual_path.push_back(i);
+    }
+    REQUIRE(test_path == actual_path);
+}
