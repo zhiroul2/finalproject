5d4cdbed2a73a5208b7ad966952b37fe6c0e034a
Chan, Muk Yeh <mukyehc2@illinois.edu>
2022-04-20 22:55:15 -0500

    Update dataTest.cpp

--- Code/tests/dataTest.cpp
+++ Code/tests/dataTest.cpp
@@ -31,5 +31,27 @@ TEST_CASE("files successfully loaded, check for all positive data", "[nodeData()
 
 
 TEST_CASE("files successfully loaded, check for all positive data for edge", "[edgeData()]") {
-   
+   map<int, EdgeData> test = edgeData("./Data/EdgeDataCorrection.txt");
+    REQUIRE(test.size() == 4);
+    SECTION("coordinates and distances are all positive") {
+        bool tf = true;
+        for (unsigned i = 0; i < test.size(); i++) {
+            if (test[i].startNode < 0 || test[i].endNode < 0 || test[i].distance < 0 || test[i].startNode == test[i].endNode) {
+                tf = false;
+                break;
+            }
+        }
+        REQUIRE(tf == true);
+    }
+ 
+    SECTION("edgeID are all positive") {
+        bool tf = true;
+        for (auto it = test.begin(); it != test.end(); ++it) {
+            if (it->first < 0) {
+                tf = false;
+                break;
+            }
+        }
+        REQUIRE(tf == true);
+    }
 }
